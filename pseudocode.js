44d16d577bc9451b7da69997641c9717a3c0450b
michael-wiparata <mikewipa@gmail.com>
2019-05-17 15:35:17 +1200

    apply JScode

--- pseudocode.js
+++ pseudocode.js
@@ -1,21 +1,79 @@
 //object.addEventListener(event, function)
-var One = document.getElementById('1');
-var Two = document.getElementById('2');
-var Three = document.getElementById('3');
+// var One = document.getElementById('1');
+// One.addEventListener('click', run1);
 
 
-One.addEventListener('click', hi);
-Two.addEventListener('click', hello);
-Three.addEventListener('click', Yo);
+function run1(){
 
-function hi() {
-    alert('hi')
-}
-function hello() {
-    alert('hello')
-}
-function Yo() {
-    alert('Yo')
+	document.case.code.value += "1"
+};
+function run2(){
+
+	document.case.code.value += "2"
+};
+function run3(){
+
+	document.case.code.value += "3"
+};
+function run4(){
+
+	document.case.code.value += "4"
+};
+function run5(){
+
+	document.case.code.value += "5"
+};
+
+function run6(){
+
+	document.case.code.value += "6"
+};
+
+function run7(){
+
+	document.case.code.value += "7"
+};
+
+function run8(){
+
+	document.case.code.value += "8"
+};
+
+function run9(){
+
+	document.case.code.value += "9"
+};
+
+function run0(){
+
+	document.case.code.value += "0"
+};
+function runPlus(){
+
+	document.case.code.value += "+"
+};
+function runMinus(){
+
+	document.case.code.value += "-"
+};
+function runDivide(){
+
+	document.case.code.value += "/"
+};
+function runMultiply(){
+
+	document.case.code.value += "*"
+};
+
+function runC(){
+
+	document.case.code.value = ""
+};
+
+
+function runEquals() {
+	var equals = eval(document.case.code.value)
+	document.case.code.value = equals;
 }
 
 
